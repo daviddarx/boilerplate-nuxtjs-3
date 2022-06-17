1749628f1143edb34f43883088c475fa383eacd6
David Darx <david.darx@liip.ch>
2022-06-17 19:53:21 +0200

    feat(directus) add basic directus demo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,8 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['nuxt-directus', '@nuxtjs/tailwindcss'],
+  directus: {
+    url: 'https://dex2bcoq.directus.app',
+  },
 })
