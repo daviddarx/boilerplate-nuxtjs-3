0d1652c5296677578a4d0cc87c0a845146e7ab81
David Darx <david.darx@liip.ch>
2022-06-24 12:49:20 +0200

    feat(blog) remove console log to fix reload error

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -37,7 +37,7 @@ const { data: postsResponse } = await useFetch(
 export default {
   name: 'BlogPage',
   mounted() {
-    console.log('blog page mounted', this.postsResponse.data.collectionblog[0])
+    // console.log('blog page mounted', this.postsResponse.data.collectionblog[0])
   },
 }
 </script>
