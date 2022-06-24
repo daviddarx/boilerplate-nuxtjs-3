cdb1612a4dd8f2d87e8f21b363a4092aa6927db8
David Darx <david.darx@liip.ch>
2022-06-24 15:37:15 +0200

    feat(env) debug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from 'nuxt'
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss'],
-  publicRuntimeConfig: {
+  runtimeConfig: {
     BACKEND_BASE_URL: process.env.BACKEND_BASE_URL,
   },
 })
